8fa5a69a654e267862e62f76822ac6ccdbc0bed2
Lucas Martins <lucas@rubcube.com>
2021-02-14 16:47:00 -0300

    resolving image problem xD

--- src/components/logo.py
+++ src/components/logo.py
@@ -6,12 +6,12 @@ class Logo(Label):
     def __init__(self, master=None):
         load = Image.open('./assets/logo.png')
         load = load.resize((80,80))
-        render = ImageTk.PhotoImage(load)
+        self.render = ImageTk.PhotoImage(load)
         Label.__init__(
             self, 
             master, 
-            bg='#000',
-            padx=10,
-            pady=10,
-            image=render
+            # bg='#000',
+            padx=13,
+            pady=13,
+            image=self.render
         )
